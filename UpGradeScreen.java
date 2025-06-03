ef48b57a97d0f24fa684d61808e9a6e1a5deb4d8
xxMichaelxx1 <woushinidie@gmail.com>
2025-06-03 09:27:48 -0400

    Some comments

--- UpGradeScreen.java
+++ UpGradeScreen.java
@@ -26,7 +26,7 @@ public class UpGradeScreen extends World
     public UpGradeScreen(int coin,int hp,int rateOfFire,int damage,int speed,int numOfJet2)
     {    
         
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world with 400x750 cells with a cell size of 1x1 pixels.
         super(400, 750, 1); 
         this.hp=hp;
         this.coin=coin;
@@ -37,35 +37,56 @@ public class UpGradeScreen extends World
         this.numOfJet2= numOfJet2;
         restartGame = new RestartGame(coin,hp,rateOfFire,damage,speed,numOfJet2);
         addObject(restartGame,200,580);
-        HP hP = new HP();
         setUp();
+        
+        // Add HP image into up grade world
+        HP hP = new HP();
         addObject(hP,30,80);
+        
+        // Add fire rate into up grade world
         fireRate FR = new fireRate();
         addObject(FR,25,180);
+        
+        // Add damage image into up grade world
         damage dmg = new damage();
         addObject(dmg,30,280);
+        
+        // Add speed image into up grade world
         speed spd = new speed();
         addObject(spd,30,380);
+        
+        // Add HP label into up grade world
         Label label = new Label("HP", 30);
         addObject(label,85,120);
+        
+        // Add fire rate label into up grade world
         Label label1 = new Label("Fire Rate", 30);
         addObject(label1,85,220);
+        
+        // Add damage label into up grade world
         Label label2 = new Label("Damage", 30);
         addObject(label2,85,320);
+        
+        // Add speed label into up grade world
         Label label3 = new Label("Speed", 30);
         addObject(label3,85,420);
+        
+        // Add five up grade buttons
         for(int i=0; i<5;i++){
             buttun[i] = new PropertyUpGradeButton();
             addObject(buttun[i], 80,80+100*i);
         }
     }
+    
     public static void upGrade(PropertyUpGradeButton x){
+        // Add 20 hp when the first button is pressed
         if (x==buttun[0]){
             if (hp < 100)
             {
                 hp+=20;
             }
         }
+        // Add fire rate when the second button is pressed
         if (x==buttun[1]){
             if (rateOfFire > 1)
             {
